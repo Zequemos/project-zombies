aa250bf7cbb18bdca0fd1ea6be0817d7325b9487
Zeque <zequeemos@gmail.com>
2014-12-19 14:50:48 +0100

    Proyecto de Unity subido
    En este commit subo el proyecto de Unity, con todos los archivos. Que
    aproveche :+1:

---
+++ Zombies/Assets/Scripts/GoTo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoTo : MonoBehaviour {
+
+	public Transform objective;
+	private NavMeshAgent navmesh;
+
+	// Use this for initialization
+	void Start () {
+		navmesh = GetComponent<NavMeshAgent> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		navmesh.SetDestination (objective.position);
+	}
+
+	void OnGUI() {
+		GUI.Label(new Rect(10, 10, 100, 20), "Hello World!");
+	}
+}
+
